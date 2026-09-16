a8c2e23f39ceb4b1522dfab46457dc1366d86b57
UndefiendUserNull <viski8548@gmail.com>
2026-09-17 00:55:49 +0300

    Add rom loading

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
 ﻿namespace Chip80808
 {
-    internal class Program
+    internal static class Program
     {
-        byte[] memory = new byte[4096];
+        static byte[] memory = new byte[4096];
 
         static void Main(string[] args)
         {
-            var romPath = args[1];
-            Console.Write(File.ReadAllBytes(romPath));
+            var romPath = args[0];
+            var rom = File.ReadAllBytes(romPath);
+            Array.Copy(rom, 0, memory, 0x200, rom.Length);
+
+
+            for (int i = 0; i < memory.Length; i++)
+            {
+                Console.WriteLine($"{i:X4}: {memory[i]:X2}");
+            }
         }
     }
 }
