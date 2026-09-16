8765fa54a4a663cb3c8907b64a7382a492ef2ad2
UndefiendUserNull <viski8548@gmail.com>
2026-09-17 00:25:15 +0300

    Initial files

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Chip80808
+{
+    internal class Program
+    {
+        byte[] memory = new byte[4096];
+
+        static void Main(string[] args)
+        {
+            var romPath = args[1];
+            Console.Write(File.ReadAllBytes(romPath));
+        }
+    }
+}
