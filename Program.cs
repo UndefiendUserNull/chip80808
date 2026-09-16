8cf6a099a596af4e8061da87d1041d0016440722
UndefiendUserNull <viski8548@gmail.com>
2026-09-17 02:12:16 +0300

    Basic instructions and deocoding

--- Program.cs
+++ Program.cs
@@ -2,19 +2,13 @@
 {
     internal static class Program
     {
-        static byte[] memory = new byte[4096];
+        static Chip8 chip = new();
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var romPath = args[0];
-            var rom = File.ReadAllBytes(romPath);
-            Array.Copy(rom, 0, memory, 0x200, rom.Length);
 
-
-            for (int i = 0; i < memory.Length; i++)
-            {
-                Console.WriteLine($"{i:X4}: {memory[i]:X2}");
-            }
+            chip.Start(romPath);
         }
     }
 }
